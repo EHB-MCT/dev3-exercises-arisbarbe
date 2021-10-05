8dc7e6910bd66f4f52a9116313767f0efd9bb158
arisbarbe <aris.barbe@student.ehb.be>
2021-10-05 16:45:43 +0200

    Summit

--- S1_Exercises/src/main/kotlin/Main.kt
+++ S1_Exercises/src/main/kotlin/Main.kt
@@ -1,3 +1,19 @@
 fun main(){
-    println("Hello World")
+    //println("Hello World")
+    //val classmates = arrayOf("Aris", "Bert", "Dirk", "Pieter", "Marc")
+    val classmates: Array<String> = arrayOf("Aris", "Bert", "Dirk", "Pieter", "Marc")
+    println(print())
+}
+
+/*fun print(){
+    println(classmates[0])
+    /*for (classmate in classmates) {
+        classmate
+    }*/
+
+}*/
+
+fun print(Greetings: String): String {
+// The Unit return type means nothing to return
+    return "Did you lose the game, Cindy?"
 }
